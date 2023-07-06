601fc3e9df842e789b4c1fa38fccd48a13d6927d
boris.molchanov <boris.molchanov@itp-it.com>
2023-07-06 15:47:31 +0400

    added memorize

---
+++ memorize.js
@@ -0,0 +1,23 @@
+function memoize(func) {
+    const cache = {};
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache[key]) {
+            return cache[key];
+        }
+
+        const result = func.apply(this, args);
+        cache[key] = result;
+
+        return result;
+    };
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+// Create a memoized version of the 'add' function
+const memoizedAdd = memoize(add);
